4fd2a8f5e784b5998a59b90923dd7e4b37fdf18a
Luke Ghenco <lukeghenco@gmail.com>
2017-07-13 19:30:52 -0400

    Add Movie model, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root_path 
+  root 'movies#index'
+
+  # CRUD routes for Movie
+  resources :movies, only: [:index, :new, :create]
 end
