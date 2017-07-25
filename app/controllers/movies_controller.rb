2aca5b7551f50c884aa6e63910c4ad884836ae65
Lukeghenco <lukeghenco@gmail.com>
2017-07-25 17:58:54 -0400

    Add movies index route

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,29 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all 
+  end 
+
+  def new
+    @movie = Movie.new 
+  end
+
+  def create 
+    @movie = Movie.new(movie_params)
+    if @movie.save 
+      redirect_to @movie 
+    else 
+      render :new 
+    end
+  end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+  end
+
+  private 
+
+    def movie_params 
+      params.require(:movie).permit(:title, :release_year)
+    end
+end
